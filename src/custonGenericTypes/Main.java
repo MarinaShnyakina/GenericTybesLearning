373fa4343b868b26a8264092b50d8f3d23761109
Marina Shnyakina <schnyakina@yandex.ru>
2022-12-08 13:50:29 +0300

    Example of not working Object cast

--- src/custonGenericTypes/Main.java
+++ src/custonGenericTypes/Main.java
@@ -2,6 +2,19 @@ package custonGenericTypes;
 
 public class Main {
     public static void main(String[] args){
+        Account acc1 = new Account(12345, 5000);
+        System.out.println(acc1.getId());
+
+        Account acc2 = new Account("sid12345", 5000);
+        System.out.println(acc2.getId());
+
+        // так можно
+        int a = ((Integer) acc1.getId()) + 10;
+        System.out.println(a);
+
+        // но так нельзя, потому что там Object
+        int b = ((Integer) acc2.getId()) + 10;
+        System.out.println(b);
 
     }
 }
