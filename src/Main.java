77f78a79d23f37896b18a81431c874a959ba2371
Marina Shnyakina <schnyakina@yandex.ru>
2022-12-08 11:22:59 +0300

    Finished with instanceof

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-        Object o = new Scanner(System.in);
+       /* Object o = new Scanner(System.in);
 
 
 
@@ -20,7 +20,18 @@ public class Main {
 
         Object o1 = new String();
 
-        Object o2 = new Integer(15);
+        Object o2 = new Integer(15);*/
+
+
+
+        Object[] objects = {10, "Привет", 3.14}; //здесь происходит автоупаковка
+
+        for (Object o : objects){
+            if(o instanceof String){
+                String s = (String) o; // тут мы делаем Cast - преобразование
+                System.out.println(s);
+            }
+        }
 
 
     }
