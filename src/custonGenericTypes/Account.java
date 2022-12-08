e7d8228f2deba250b7e054370ba60f625afc8bdf
Marina Shnyakina <schnyakina@yandex.ru>
2022-12-08 14:13:53 +0300

    Refactored to Generic Class

--- src/custonGenericTypes/Account.java
+++ src/custonGenericTypes/Account.java
@@ -1,6 +1,7 @@
 package custonGenericTypes;
 
-public class Account {
+// неверный первый вариант
+/*public class Account {
     private Object id;
     private int sum;
 
@@ -23,9 +24,40 @@ public class Account {
 
     public void setSum(int sum) {
         this.sum = sum;
+    }*/
+
+    // верный второй вариант
+    // отфакторим наш код используя Generic Types
+    public class Account <T> {       // <T> - обычно так называют Generic Types
+        /* написав <T> мы уже объявили, что есть некоторый передаваемый тип,
+         который мы используем внутри нашего класса и он будет подставлен в те места, где мы этот тип используем*/
+
+        private T id;
+        private int sum;
+
+        public Account(T id, int sum) {
+            this.id = id;
+            this.sum = sum;
+        }
+
+        public T getId() {
+            return id;
+        }
+
+        public void setId(T id) {
+            this.id = id;
+        }
+
+        public int getSum() {
+            return sum;
+        }
+
+        public void setSum(int sum) {
+            this.sum = sum;
+        }
+
+
+
+
+
     }
-
-
-
-
-}
--- src/custonGenericTypes/Main.java
+++ src/custonGenericTypes/Main.java
@@ -2,19 +2,45 @@ package custonGenericTypes;
 
 public class Main {
     public static void main(String[] args){
-        Account acc1 = new Account(12345, 5000);
+
+        // неверный первый вариант
+        /*Account acc1 = new Account(12345, 5000);
         System.out.println(acc1.getId());
 
         Account acc2 = new Account("sid12345", 5000);
         System.out.println(acc2.getId());
 
         // так можно
-        int a = ((Integer) acc1.getId()) + 10;
+        int a = ((Integer) acc1.getId()) + 10; // Опасное преобразование типов
         System.out.println(a);
 
         // но так нельзя, потому что там Object
-        int b = ((Integer) acc2.getId()) + 10;
-        System.out.println(b);
+        int b = ((Integer) acc2.getId()) + 10; // Опасное преобразование типов
+        System.out.println(b);*/
 
+        // верный второй вариант
+
+        Account <Integer> acc1 = new Account(12345, 5000);
+        System.out.println(acc1.getId());
+
+        Account <String> acc2 = new Account("sid12345", 5000);
+        System.out.println(acc2.getId());
+
+        // это код с преобразованием
+        // нам IJ уже не даст допустить ошибку
+        // int b = ((Integer) acc2.getId()) + 10;
+        // System.out.println(b);
+
+        // а так мы можем, потому что асс1 - Integer, можно сложить
+        // int a = ((Integer) acc1.getId()) + 10;
+        // System.out.println(a);
+
+        // но так как преобразование нам уже не нужно, то лучше написать так
+        int a = acc1.getId() + 10;
+        System.out.println(a);
+
+        // а так нельзя
+        int b = acc2.getId() + 10;
+        System.out.println(b);
     }
 }
